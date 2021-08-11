379be24cb638d463515709906801fe79f84af1cb
balg <2700278+balg@users.noreply.github.com>
2021-08-11 18:00:24 +0200

    Exercise 1: Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -29,7 +29,19 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <ActionGroup className="desktop-and-above">
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroup>
         <Logo />
+        <SubscribeWrapper>
+          <Button>Subscribe</Button>
+          <Link>Already a subscriber?</Link>
+        </SubscribeWrapper>
       </MainHeader>
     </header>
   );
@@ -39,6 +51,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -57,6 +73,35 @@ const ActionGroup = styled.div`
   svg {
     display: block;
   }
+
+  &.desktop-and-above {
+    display: none;
+
+    @media ${QUERIES.laptopAndUp} {
+      display: flex;
+    }
+  }
+`;
+
+const SubscribeWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    position: relative;
+    display: revert;
+    justify-self: end;
+  }
+`;
+
+const Link = styled.a`
+  color: ${COLORS.gray[900]};
+  font-size: ${14 / 16}rem;
+  text-decoration: underline;
+  font-style: italic;
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  margin-top: 8px;
 `;
 
 const MainHeader = styled(MaxWidthWrapper)`
@@ -65,6 +110,20 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+    justify-content: revert;
+    margin-top: 16px;
+    margin-bottom: 72px;
+  }
 `;
 
 export default Header;
